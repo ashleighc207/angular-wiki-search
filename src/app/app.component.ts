6310666b6604719fb4ab79a9098c10c400eb3ebd
Ashleigh Christopher <ashleighc207@gmail.com>
2020-01-22 15:36:34 -0500

    get api working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { WikipediaService } from "./wikipedia.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'wiki';
+  constructor(private wikipedia: WikipediaService) {}
+  onTermChange(term: string) {
+    this.wikipedia.search(term).subscribe(res => {
+      console.log(res);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
-import { ResultsComponent } from './results/results.component';
+import { AppComponent } from "./app.component";
+import { SearchComponent } from "./search/search.component";
+import { ResultsComponent } from "./results/results.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchComponent,
-    ResultsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SearchComponent, ResultsComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
